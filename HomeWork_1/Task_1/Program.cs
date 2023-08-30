89f69f25a8a6b86ad9a90fbfa73a5d9055c90872
Artem Cherepanov <artem.cherepanov530@gmail.com>
2023-08-30 22:27:43 +0200

    Home work 1 is added

---
+++ HomeWork_1/Task_1/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+
+Console.WriteLine("Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее");
+Console.Write("Insert number 1 :");
+double number1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Insert number 2 :");
+double number2 = Convert.ToDouble(Console.ReadLine());
+if(number1 > number2)
+    Console.Write($"{number1} is bigger than {number2}");
+else
+     Console.Write($"{number2} is bigger than {number1}");
